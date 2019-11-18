0744f2231886c1eec8e251834265227b238d5079
Paola Barboglio <paola.barboglio@gmail.com>
2019-11-18 16:31:58 +0100

    add validations to dream model

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,4 +1,7 @@
 class Dream < ApplicationRecord
   has_many :bookings
   belongs_to :user
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true, uniqueness: true
+  validates :duration, presence: true
 end
