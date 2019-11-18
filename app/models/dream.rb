208c0e32fe2c4887d3772d46e83defabd0da1142
Paola Barboglio <paola.barboglio@gmail.com>
2019-11-18 15:40:12 +0100

    add Devise Gem, create User, Dream and Booking model

---
+++ app/models/dream.rb
@@ -0,0 +1,4 @@
+class Dream < ApplicationRecord
+  has_many :bookings
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_11_18_143442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.date "date"
+    t.bigint "user_id"
+    t.bigint "dream_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dream_id"], name: "index_bookings_on_dream_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "dreams", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "vibe"
+    t.string "duration"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_dreams_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "bookings", "dreams"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "dreams", "users"
 end
