f96762482080aa7b4b7cf707c35b68aef66d912c
adiam feireiss <adiamfeireiss@gmail.com>
2019-11-19 14:39:07 +0100

    added constant for vibes

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,8 +1,9 @@
 class Dream < ApplicationRecord
+  VIBES = ["floating","empowering" ,"careless", "vibrant", "optimistic", "happy", "stoned", "light" ,"fun", "realistic", "emotional" , "psychedelic", "sexual", "loving" , "healing", "futuristic", "past"]
   has_many :bookings
   belongs_to :user
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true, uniqueness: true
   validates :duration, presence: true
-  # validates :vibe, inclusion: { in: %w[floating empowering careless vibrant optimistic happy stoned light fun realistic emotional psychedelic sexual loving healing futuristic past], message:"please choose from the list" }
+  validates :vibe, inclusion: { in: VIBES, message:"please choose from the list" }
 end
