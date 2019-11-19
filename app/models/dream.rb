c0dfdc00edfef8ce48d2ca1eb43108cef4c57b01
adiam feireiss <adiamfeireiss@gmail.com>
2019-11-19 12:49:02 +0100

    updated new dream view

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -4,4 +4,5 @@ class Dream < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true, uniqueness: true
   validates :duration, presence: true
+  # validates :vibe, inclusion: { in: %w[floating empowering careless vibrant optimistic happy stoned light fun realistic emotional psychedelic sexual loving healing futuristic past], message:"please choose from the list" }
 end
