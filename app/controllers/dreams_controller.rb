0c37f49691fdd8dd10d9edf9ab9bae735ab78fa5
Susie Kim <hello@susiekim.de>
2019-11-19 11:41:08 +0100

    implement new/create dream aaction

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -7,4 +7,23 @@ class DreamsController < ApplicationController
     @dream = Dream.find(params[:id])
     authorize @dream
   end
+
+  def new
+    @dream = Dream.new
+    authorize @dream
+  end
+
+  def create
+    @dream = Dream.new(dream_params)
+    @dream.user = current_user
+    authorize @dream
+    @dream.save
+    redirect_to root_path
+  end
+
+  private
+
+  def dream_params
+    params.require(:dream).permit(:title, :description, :duration, :vibe)
+  end
 end
--- app/policies/dream_policy.rb
+++ app/policies/dream_policy.rb
@@ -8,4 +8,12 @@ class DreamPolicy < ApplicationPolicy
   def show?
     true
   end
+
+  # def new?
+  #   true
+  # end
+
+  def create?
+    true
+  end
 end
