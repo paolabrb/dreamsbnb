e367313fa380c059285be3ce4e69672b2eb817d3
Paola Barboglio <paola.barboglio@gmail.com>
2019-11-19 14:21:17 +0100

    set up a user dashboard with the posted/booked dreams

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,7 @@ class PagesController < ApplicationController
   def home
     @dreams = policy_scope(Dream)
   end
+
+  def user_profile
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/profile', to: 'pages#profile', as: 'user_profile'
   resources :dreams, only: [:show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
