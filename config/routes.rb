b790450178f911215ffee0224adfa9805e0c75bc
Isabelle Ferrier <ferrierisabelle@gmail.com>
2019-11-20 15:12:48 +0100

    Done Edit, Update  and Destroy methods in booking controller, view in user profile

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'pages#profile', as: 'user_profile'
   delete 'dreams/:id', to: 'dreams#destroy', as: 'delete_dream'
   resources :dreams, except: [:destroy] do
+    # delete '/dreams/:dream_id/bookings/:id', to: 'bookings#destroy', as: 'delete_booking'
     resources :bookings
   end
 end
