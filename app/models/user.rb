8ef932da141a152b07b576d40d442c84fa5ec9c7
adiam feireiss <adiamfeireiss@gmail.com>
2019-11-20 12:55:02 +0100

    user-profile-implement

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :dreams
   has_many :bookings
+  mount_uploader :photo, PhotoUploader
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
