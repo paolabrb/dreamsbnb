be18fe35142fa995e1c550de73030a40e843e66e
Paola Barboglio <paola.barboglio@gmail.com>
2019-11-21 15:14:17 +0100

    add nickname field to registration, permit nickname parameter when registering

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,4 +1,5 @@
 class DreamsController < ApplicationController
+  before_action :authenticate_user!
   def index
     # if params[:query].present?
     #   @dreams = Dream.search_by_vibe(params[:query])
