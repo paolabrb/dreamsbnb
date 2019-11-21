c4f80cc1b14eb56b9a758930f8ebc1b469dde251
Isabelle Ferrier <ferrierisabelle@gmail.com>
2019-11-21 16:30:40 +0100

    User Profile Editing done: avatar upload and nickname change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
+    authorize @user
   end
 
   def update
     @user = User.find(params[:id])
+    @user.update(user_params)
+    authorize @user
+    @user.save
+    redirect_to user_profile_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:photo, :nickname)
   end
 end
