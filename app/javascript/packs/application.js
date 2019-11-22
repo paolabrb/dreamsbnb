2a8bff565e20fc0f443c8f6617d088aece245c2c
Susie Kim <hello@susiekim.de>
2019-11-22 12:50:47 +0100

    yes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,14 @@
 import "bootstrap";
 import "../plugins/flatpickr"
+
+
+
+
+  const editButton = document.querySelector('.edit-button');
+  const editForm = document.querySelector('.edit-form');
+
+  editButton.addEventListener('click', (e) => {
+     editForm.style.visibility = 'initial';
+  })
+
+
